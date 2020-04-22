e0dbe25835818b1a8ec83df0cd710ccd05bfc06d
chrisarm <chrisarm@gmail.com>
2020-04-22 03:35:16 -0700

    chocopack2

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -1,6 +1,6 @@
 :: Ensure C:\Chocolatey\bin is on the path
-set /p PATH=%PATH%;C:\ProgramData\chocolatey\
-echo %PATH%
+:: set /p PATH=%PATH%;C:\ProgramData\chocolatey\
+:: echo %PATH%
 
 :: Install all the things; for example:
 choco source add REPOSITORY_INFO_HERE
